6c510af4e40b9f81fb626e2bf7f0451dd984f4da
AlaEddineDammak <alaindammak@gmail.com>
2025-01-16 21:05:50 +0100

    fixing views + viewModel

--- ServiceApresVenteApp/ViewModels/LoginViewModel.cs
+++ ServiceApresVenteApp/ViewModels/LoginViewModel.cs
@@ -6,9 +6,11 @@ namespace ServiceApresVenteApp.ViewModels
     {
         [Required]
         [EmailAddress]
+        // [RegularExpression(@"^[a-zA-Z0-9_]+@[a-zA-Z0-9_]+\.[a-zA-Z0-9_]+$")]
         public string Email { get; set; }
         [Required]
         [DataType(DataType.Password)]
+        // [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$")]
         public string Password { get; set; }
         [Display(Name = "Remember me")]
         public bool RememberMe { get; set; }
